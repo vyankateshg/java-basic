008b3fdad591be6cf2cabca75edcd2be111664d4
vyankatesh gupta <vyankateshg17@gmail.com>
2025-01-09 16:37:00 +0530

    Create first.java

---
+++ first.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Print "Hello, World!" to the console
+        System.out.println("Hello, World!");
+        System.out.println("radhe radhe, World!");
+
+    }
+}
